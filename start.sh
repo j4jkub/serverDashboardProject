5e9989f191fcc9c89e335a1908c16530b82f6ce7
Jakub Pączko <jakub.paczko01@gmail.com>
2026-07-15 19:10:16 +0200

    .gitignore file typo

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+#run redis
 sudo service redis-server start
+
+
+# start djano server
+cd backend
+source .venv/bin/activate
+python manage.py runserver &
+cd ..
+
+# start react server
+cd frontend
+npm start &
+cd ..
